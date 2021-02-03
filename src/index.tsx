c7ca9653333906268fdb877717395c9ef1de09dd
Alex Ilchenko <alex@swing.dev>
2021-02-04 00:12:24 +0100

    config: sample React application

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import { App } from '~/App';
+
+import './style.css';
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
