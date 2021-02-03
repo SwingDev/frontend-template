b84b31df5d1075437800b92a895dd8605ec75597
Alex Ilchenko <alex@swing.dev>
2021-02-04 00:08:12 +0100

    config: added snowpack plugins, config and type definition

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,16 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
+  alias: {
+    '~': './src',
+  },
   mount: {
-    /* ... */
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
