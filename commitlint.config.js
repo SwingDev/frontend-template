8a232a67e4f78d9b09c037b10507f44f490e40f9
Alex Ilchenko <alex@swing.dev>
2021-02-04 21:01:40 +0100

    config(CommitLint): force passing scope in commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,7 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'footer-max-line-length': [2, 'always', 100],
     'header-max-length': [2, 'always', 100],
+    'scope-empty': [2, 'never'],
     'scope-case': [2, 'always', ['upper-case', 'sentence-case']],
     'subject-case': [
       2,
